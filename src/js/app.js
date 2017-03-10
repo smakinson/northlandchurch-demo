334eea96861fc05ab0983bc8232ac621d2c0dede
Shawn Makinson <smakinson@gmail.com>
2017-03-10 10:38:23 -0500

    Top nav changed to javascript based
    In order to reuse the dropdowns on the small screen I needed to use javascript instead of the hide-dhild css from tachyons. Also a few header responsive fixes for ie9.

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,61 @@
+
+
 ;(function ($) {
 
-$('#search-bar-btn').click(() => {
-  $('#search-bar').removeClass('closed');
-  return false;
-});
+  /* Top search bar */
+  $('#search-bar-btn').click(() => {
+    $('#search-bar').removeClass('closed');
+    return false;
+  });
 
-$('#search-bar-close-btn').click(() => {
-  $('#search-bar').addClass('closed');
-  return false;
-});
+  $('#search-bar-close-btn').click(() => {
+    $('#search-bar').addClass('closed');
+    return false;
+  });
+
+  /* Top nav */
+
+  // TODO: Make sur ethis matches the CSS size var.
+  var notSmallScreenMediaQuery = "(min-width: 30em)";
+
+  function openNavMenu(menu) {
+    menu.addClass('open');
+  }
+
+  function closeNavMenu(menu) {
+    menu.removeClass('open');
+  }
+
+  $('.top-nav-menu-trigger').hover(function(e) {
+    // No hovering on the small screen.
+    if(Modernizr.mq(notSmallScreenMediaQuery)){
+      //$(this).find('.top-nav-menu').addClass('open');
+      openNavMenu($(this).find('.top-nav-menu'));
+    }
+  },function () {
+    //$(this).parent().find('.top-nav-menu').removeClass('open');
+    closeNavMenu($(this).parent().find('.top-nav-menu'));;
+  });
+
+  $('.top-nav-menu-trigger').click(function(e) {
+    //$(this).parent().find('.top-nav-menu').addClass('open');
+    //$(this).find('.top-nav-menu').addClass('open');
+    openNavMenu($(this).find('.top-nav-menu'));
+    return false;
+  });
+
+  $('#top-nav .top-nav-menu a').click(function(e) {
+    // Make sure the menu is fully visible before allowing links to work
+    if ($(this).parent().css('opacity') < 1){
+      return false;
+    }
+  });
+
+  $('.top-nav-menu-close-btn').click(function(e) {
+    // Close the menu
+    //$(this).parent().removeClass('open');
+    closeNavMenu($(this).parent());
+    return false;
+  });
 
 }($));

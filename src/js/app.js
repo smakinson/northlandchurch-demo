57135ac6949d404dcb248246f84c84a75300b3c5
Shawn Makinson <smakinson@gmail.com>
2017-03-10 10:57:07 -0500

    Some top nav adjustments for ie8

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,3 @@
-
-
 ;(function ($) {
 
   /* Top search bar */
@@ -26,38 +24,32 @@
     menu.removeClass('open');
   }
 
-  $('.top-nav-menu-trigger').hover(function(e) {
+  $('.top-nav-menu-trigger').hover(function (e) {
     // No hovering on the small screen.
-    if(Modernizr.mq(notSmallScreenMediaQuery)){
-      //$(this).find('.top-nav-menu').addClass('open');
+    if (Modernizr.mq(notSmallScreenMediaQuery)) {
       openNavMenu($(this).find('.top-nav-menu'));
     }
-  },function () {
-    //$(this).parent().find('.top-nav-menu').removeClass('open');
-    closeNavMenu($(this).parent().find('.top-nav-menu'));;
+  }, function () {
+    closeNavMenu($(this).parent().find('.top-nav-menu'));
   });
 
-  $('.top-nav-menu-trigger').click(function(e) {
-    //$(this).parent().find('.top-nav-menu').addClass('open');
-    //$(this).find('.top-nav-menu').addClass('open');
+  $('.top-nav-menu-trigger').click(function (e) {
     openNavMenu($(this).find('.top-nav-menu'));
-    return false;
   });
 
-  $('#top-nav .top-nav-menu a').click(function(e) {
+
+  $('#top-nav .top-nav-menu a').click(function (e) {
     // Make sure the menu is fully visible before allowing links to work,
     // but make sure opacity is supported first.
     if($('html').hasClass('opacity')) {
       if ($(this).parent().css('opacity') < 1) {
-        alert('dsdsf');
         return false;
       }
     }
   });
 
-  $('.top-nav-menu-close-btn').click(function(e) {
+  $('.top-nav-menu-close-btn').click(function (e) {
     // Close the menu
-    //$(this).parent().removeClass('open');
     closeNavMenu($(this).parent());
     return false;
   });
